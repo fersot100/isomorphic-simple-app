8108cc6e1023c95156581b0633bf702b20a640a7
Juan Erazo <fersot100@yahoo.com>
2018-01-30 15:35:17 -0800

    Isomorphic simple app

---
+++ src/client/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <div>I'm the home component</div>
+            <button onClick={() => console.log('Hi There!')}>Press Me</button>
+        </div>
+    );
+}
+
+
+export default Home;
